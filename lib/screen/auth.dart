49e5384351a520d4795299ff9a52488c223655c6
Darpan <darpanneve3@gmail.com>
2024-01-01 23:24:51 +0530

    minor changes

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -8,6 +8,19 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _email = '';
+  var _password = '';
+  bool _isLogin = true;
+  void _submit() {
+    final isValid=_formKey.currentState!.validate();
+    if(!isValid){
+      _formKey.currentState!.save();
+
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,8 +41,52 @@ class _AuthScreenState extends State<AuthScreen> {
                 ),
               ),
               const Text('Welcome to Flutter Chat'),
+              Form(
+                key: _formKey,
+                child: Card(
+                  margin: const EdgeInsets.all(15),
+                  color: Theme.of(context).colorScheme.tertiaryContainer,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          decoration: const InputDecoration(labelText: 'Email'),
+                          textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value!.isEmpty || !value.contains('@')) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _email = value!;
+                          },
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value!.isEmpty || value.length < 7 ) {
+                              return 'Password must be at least 7 characters long.';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _password = value!;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submit,
                 child: const Text('Login'),
               ),
               ElevatedButton(
